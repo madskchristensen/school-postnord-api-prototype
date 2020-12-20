9c6c8dc2b2b59ec48c51818e2752c8ab669c04db
Mads <maller2650@gmail.com>
2020-12-20 20:36:49 +0100

    -- fjernet unødvendige kommentarer

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,15 +2,6 @@ import com.google.gson.*;
 
 import java.io.*;
 
-/*
-    1. Find en måde at håndtere JSON i java. F.eks. via et library
-    2. Mulighed for at generere JSON i java. F.eks. hvis library kan konvertere objekter til JSON:
-        i. Lav objekt(er)
-        ii. Konverter objekt(er) til JSON
-    3. Send JSON + api key til EDI endpoint der returnerer PDF
-    4. Find en måde at læse værdier fra JSON. I dette tilfælde evt. gem de forskellige relevante værdier + hent base64 string og lav pdf af den
- */
-
 public class Main {
     public static void main(String[] args) {
         File jsonFile = new File("./resources/jsonEdiRequest.json");
